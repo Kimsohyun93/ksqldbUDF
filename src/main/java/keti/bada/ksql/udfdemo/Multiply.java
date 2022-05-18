cce067a2d6252823023b5341ca1ec3182e646311
Kimsohyun93 <shk980903@naver.com>
2022-05-18 14:43:04 +0900

    modify code

--- src/main/java/keti/bada/ksql/udfdemo/Multiply.java
+++ src/main/java/keti/bada/ksql/udfdemo/Multiply.java
@@ -2,27 +2,28 @@ package keti.bada.ksql.udfdemo;
 
 import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
 
 @UdfDescription(name = "multiply", description = "multiplies 2 numbers")
 public class Multiply {
 
   @Udf(description = "multiply two non-nullable INTs.")
-  public long multiply(final int v1, final int v2) {
+  public long multiply(@UdfParameter int v1, @UdfParameter int v2) {
     return v1 * v2;
   }
 
   @Udf(description = "multiply two non-nullable BIGINTs.")
-  public long multiply(final long v1, final long v2) {
+  public long multiply(@UdfParameter long v1, @UdfParameter long v2) {
     return v1 * v2;
   }
 
   @Udf(description = "multiply two nullable BIGINTs. If either param is null, null is returned.")
-  public Long multiply(final Long v1, final Long v2) {
+  public Long multiply(@UdfParameter Long v1, @UdfParameter Long v2) {
     return v1 == null || v2 == null ? null : v1 * v2;
   }
 
   @Udf(description = "multiply two non-nullable DOUBLEs.")
-  public double multiply(final double v1, final double v2) {
+  public double multiply(@UdfParameter double v1, @UdfParameter double v2) {
     return v1 * v2;
   }
 }
