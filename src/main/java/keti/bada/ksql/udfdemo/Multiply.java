69fede907aab7ce0e04be606c17e403f32e92c6a
Kimsohyun93 <shk980903@naver.com>
2022-05-18 17:29:59 +0900

    modify UdfParameter

--- src/main/java/keti/bada/ksql/udfdemo/Multiply.java
+++ src/main/java/keti/bada/ksql/udfdemo/Multiply.java
@@ -9,22 +9,22 @@ import io.confluent.ksql.function.udf.UdfParameter;
 public class Multiply {
 
   @Udf(description = "multiply two non-nullable INTs.")
-  public long multiply(@UdfParameter int v1, @UdfParameter int v2) {
+  public long multiply(@UdfParameter(value = "v1", description = "first value to multiply") int v1, @UdfParameter(value = "v2", description = "second value to multiply") int v2) {
     return v1 * v2;
   }
 
   @Udf(description = "multiply two non-nullable BIGINTs.")
-  public long multiply(@UdfParameter long v1, @UdfParameter long v2) {
+  public long multiply(@UdfParameter(value = "v1", description = "first value to multiply") long v1, @UdfParameter(value = "v2", description = "second value to multiply") long v2) {
     return v1 * v2;
   }
 
   @Udf(description = "multiply two nullable BIGINTs. If either param is null, null is returned.")
-  public Long multiply(@UdfParameter Long v1, @UdfParameter Long v2) {
+  public Long multiply(@UdfParameter(value = "v1", description = "first value to multiply") Long v1, @UdfParameter(value = "v2", description = "second value to multiply") Long v2) {
     return v1 == null || v2 == null ? null : v1 * v2;
   }
 
   @Udf(description = "multiply two non-nullable DOUBLEs.")
-  public double multiply(@UdfParameter double v1, @UdfParameter double v2) {
+  public double multiply(@UdfParameter(value = "v1", description = "first value to multiply") double v1, @UdfParameter(value = "v2", description = "second value to multiply") double v2) {
     return v1 * v2;
   }
 }
