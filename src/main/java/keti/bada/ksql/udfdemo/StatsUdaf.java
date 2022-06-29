8f83d334b02ad3cb1fe9b2e68c8fa83ac5e597b2
Kimsohyun93 <shk980903@naver.com>
2022-06-29 14:58:08 +0900

    Debug

--- src/main/java/keti/bada/ksql/udfdemo/StatsUdaf.java
+++ src/main/java/keti/bada/ksql/udfdemo/StatsUdaf.java
@@ -20,7 +20,6 @@ public class StatsUdaf {
 
   public static final Schema PARAM_SCHEMA = SchemaBuilder.struct().optional()
           .field("WSTART", Schema.OPTIONAL_STRING_SCHEMA)
-          .field("AVG", Schema.OPTIONAL_FLOAT64_SCHEMA)
           .build();
 
   public static final String PARAM_SCHEMA_DESCRIPTOR = "STRUCT<" +
