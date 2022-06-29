e67ca06010d338e5922ce9b55af0502b8e48197d
Kimsohyun93 <shk980903@naver.com>
2022-06-29 14:34:38 +0900

    Debug

--- src/main/java/keti/bada/ksql/udfdemo/StatsUdaf.java
+++ src/main/java/keti/bada/ksql/udfdemo/StatsUdaf.java
@@ -9,18 +9,26 @@ import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Schema;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @UdafDescription(name = "stats",
         author = "example user",
         version = "1.3.5",
         description = "Maintains statistical values.")
 public class StatsUdaf {
 
+  private static final String WSTART = "WSTART";
+  private static final String AVG = "AVG";
+
   public static final Schema PARAM_SCHEMA = SchemaBuilder.struct().optional()
-          .field("C", Schema.OPTIONAL_INT64_SCHEMA)
+          .field(WSTART, Schema.OPTIONAL_STRING_SCHEMA)
+          .field(AVG, Schema.OPTIONAL_FLOAT64_SCHEMA)
           .build();
 
   public static final String PARAM_SCHEMA_DESCRIPTOR = "STRUCT<" +
-          "C BIGINT" +
+          "WSTART STRING" +
+          "AVG DOUBLE" +
           ">";
 
   public static final Schema AGGREGATE_SCHEMA = SchemaBuilder.struct().optional()
@@ -36,18 +44,13 @@ public class StatsUdaf {
           ">";
 
   public static final Schema RETURN_SCHEMA = SchemaBuilder.struct().optional()
-          .field("MIN", Schema.OPTIONAL_INT64_SCHEMA)
-          .field("MAX", Schema.OPTIONAL_INT64_SCHEMA)
-          .field("COUNT", Schema.OPTIONAL_INT64_SCHEMA)
-          .field("DIFFERENTIAL", Schema.OPTIONAL_INT64_SCHEMA)
+          .field(WSTART, Schema.OPTIONAL_STRING_SCHEMA)
           .build();
 
   public static final String RETURN_SCHEMA_DESCRIPTOR = "STRUCT<" +
-          "MIN BIGINT," +
-          "MAX BIGINT," +
-          "COUNT BIGINT," +
-          "DIFFERENTIAL BIGINT" +
+          "WSTART STRING" +
           ">";
+
 
   private StatsUdaf() {
   }
@@ -56,80 +59,71 @@ public class StatsUdaf {
           paramSchema = PARAM_SCHEMA_DESCRIPTOR,
           aggregateSchema = AGGREGATE_SCHEMA_DESCRIPTOR,
           returnSchema = RETURN_SCHEMA_DESCRIPTOR)
-  public static Udaf<Struct, Struct, Struct> createUdaf() {
+  public static Udaf<Struct, Map<String, Double>, Struct> createUdaf() {
     return new StatsUdafImpl();
   }
 
-  private static class StatsUdafImpl implements Udaf<Struct, Struct, Struct> {
+  private static class StatsUdafImpl implements Udaf<Struct, Map<String, Double>, Struct> {
 
     @Override
-    public Struct initialize() {
-      return new Struct(AGGREGATE_SCHEMA);
+    public Map<String, Double> initialize() {
+      System.out.println("INITIALIZE Stats Data");
+      final Map<String, Double> data = new HashMap<>();
+      data.put("1900-01-01 00:00:00 +0900", 0.0);
+      System.out.println(data);
+      return data;
     }
 
     @Override
-    public Struct aggregate(Struct newValue, Struct aggregateValue) {
-      long c = newValue.getInt64("C");
+    public Map<String, Double> aggregate(Struct newValue, Map<String, Double> aggregateValue) {
+      Double AVG = newValue.getFloat64("AVG");
+      Double WSTART = newValue.getFloat64("WSTART");
 
-      long min = Math.min(c, getMin(aggregateValue));
-      long max = Math.max(c, getMax(aggregateValue));
-      long count = (getCount(aggregateValue) + 1);
-
-      aggregateValue.put("MIN", min);
-      aggregateValue.put("MAX", max);
-      aggregateValue.put("COUNT", count);
 
       return aggregateValue;
     }
 
     @Override
-    public Struct map(Struct intermediate) {
+    public Struct map(Map<String, Double> intermediate) {
       Struct result = new Struct(RETURN_SCHEMA);
 
-      long min = intermediate.getInt64("MIN");
-      long max = intermediate.getInt64("MAX");
-
-      result.put("MIN", min);
-      result.put("MAX", max);
-      result.put("COUNT", intermediate.getInt64("COUNT"));
-      result.put("DIFFERENTIAL", max - min);
 
       return result;
     }
 
     @Override
-    public Struct merge(Struct aggOne, Struct aggTwo) {
+    public Map<String, Double> merge(Map<String, Double> aggOne, Map<String, Double> aggTwo) {
       return aggOne;
     }
-
-    private Long getMin(Struct aggregateValue) {
-      Long result = aggregateValue.getInt64("MIN");
-
-      if (result != null) {
-        return result;
-      } else {
-        return Long.MAX_VALUE;
-      }
-    }
-
-    private Long getMax(Struct aggregateValue) {
-      Long result = aggregateValue.getInt64("MAX");
-
-      if (result != null) {
-        return result;
-      } else {
-        return Long.MIN_VALUE;
-      }
-    }
-
-    private Long getCount(Struct aggregateValue) {
-      Long result = aggregateValue.getInt64("COUNT");
-
-      if (result != null) {
-        return result;
-      } else {
-        return 0L;
-      }
-    }
+//
+//    private Long getMin(Struct aggregateValue) {
+//      Long result = aggregateValue.getInt64("MIN");
+//
+//      if (result != null) {
+//        return result;
+//      } else {
+//        return Long.MAX_VALUE;
+//      }
+//    }
+//
+//    private Long getMax(Struct aggregateValue) {
+//      Long result = aggregateValue.getInt64("MAX");
+//
+//      if (result != null) {
+//        return result;
+//      } else {
+//        return Long.MIN_VALUE;
+//      }
+//    }
+//
+//    private Long getCount(Struct aggregateValue) {
+//      Long result = aggregateValue.getInt64("COUNT");
+//
+//      if (result != null) {
+//        return result;
+//      } else {
+//        return 0L;
+//      }
+//    }
   }
 }
