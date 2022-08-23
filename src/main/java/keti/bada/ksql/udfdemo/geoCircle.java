5beab21434839a54ffb411237eb9d2e800866310
Kimsohyun93 <shk980903@naver.com>
2022-08-23 11:39:51 +0900

    variations is not shown

--- src/main/java/keti/bada/ksql/udfdemo/geoCircle.java
+++ src/main/java/keti/bada/ksql/udfdemo/geoCircle.java
@@ -12,34 +12,34 @@ import com.vividsolutions.jts.util.GeometricShapeFactory;
 public class geoCircle {
 
   @Udf(description = "Create GeoJson Polygon with two inputs (lat,lng) pairs and radius. All Input types are Double.")
-  public Polygon geocircle(@UdfParameter(value = "lat", description = "the latitude of the center of the circle") double lat,
+  public Polygon geo_circle(@UdfParameter(value = "lat", description = "the latitude of the center of the circle") double lat,
                            @UdfParameter(value = "lng", description = "the longitude of the center of the circle") double lng,
                            @UdfParameter(value = "radius", description = "radius of the circle") double radius) {
     GeometricShapeFactory shapeFactory = new GeometricShapeFactory();
-    shapeFactory.setNumPoints(32);
-    shapeFactory.setCentre(new Coordinate(lat, lng));
-
-    // Length in meters of 1° of latitude = always 111.32 km
-    shapeFactory.setWidth(radius/111320d);
-    // Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
-    shapeFactory.setHeight(radius / (40075000 * Math.cos(Math.toRadians(lat)) / 360));
+//    shapeFactory.setNumPoints(32);
+//    shapeFactory.setCentre(new Coordinate(lat, lng));
+//
+//    // Length in meters of 1° of latitude = always 111.32 km
+//    shapeFactory.setWidth(radius/111320d);
+//    // Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
+//    shapeFactory.setHeight(radius / (40075000 * Math.cos(Math.toRadians(lat)) / 360));
 
     return shapeFactory.createCircle();
   }
 
   @Udf(description = "Create GeoJson Polygon with two inputs (lat,lng) pairs and radius. All Input types are Double.")
-  public Polygon geocircle(@UdfParameter(value = "lat", description = "the latitude of the center of the circle") double lat,
+  public Polygon geo_circle(@UdfParameter(value = "lat", description = "the latitude of the center of the circle") double lat,
                            @UdfParameter(value = "lng", description = "the longitude of the center of the circle") double lng,
                            @UdfParameter(value = "radius", description = "the radius of the circle") double radius,
                            @UdfParameter(value = "num_points", description = "the number of points ") int num_points) {
     GeometricShapeFactory shapeFactory = new GeometricShapeFactory();
-    shapeFactory.setNumPoints(num_points);
-    shapeFactory.setCentre(new Coordinate(lat, lng));
-
-    // Length in meters of 1° of latitude = always 111.32 km
-    shapeFactory.setWidth(radius/111320d);
-    // Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
-    shapeFactory.setHeight(radius / (40075000 * Math.cos(Math.toRadians(lat)) / 360));
+//    shapeFactory.setNumPoints(num_points);
+//    shapeFactory.setCentre(new Coordinate(lat, lng));
+//
+//    // Length in meters of 1° of latitude = always 111.32 km
+//    shapeFactory.setWidth(radius/111320d);
+//    // Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
+//    shapeFactory.setHeight(radius / (40075000 * Math.cos(Math.toRadians(lat)) / 360));
 
     return shapeFactory.createCircle();
   }
